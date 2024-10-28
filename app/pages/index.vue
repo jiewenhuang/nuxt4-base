49d60c4c9117a08826ae8c642447f3f7b990f40a
jiewenhuang <tyubar13@gmail.com>
2024-10-28 18:53:38 +0800

    initial：base project

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '~/components/ui/button'
+import { useCounterStore } from '~/stores/counter'
+import { getPostList } from '~/service/foo'
 
-<template>$END$</template>
+const counterStore = useCounterStore()
+const { data, error } = await getPostList()
+console.log(data.value, error)
+</script>
+
+<template>
+  <div class="flex justify-center h-screen w-full">
+    <div class="text-center space-y-3 font-bold">
+      <h1 class="text-3xl text-green-400">Nuxt4-base</h1>
+      <div>
+        <p class="text-base text-gray-500">Content goes here</p>
+      </div>
+      <div>
+        <p class="text-base text-gray-500">Counter: {{ counterStore.count }}</p>
+      </div>
+      <Button @click="counterStore.increment">Counter ++</Button>
+      <div v-if="!(data == null)">
+        <div v-for="item in data.items" :key="item.metadata.name">
+          <p>{{ item.spec.title }}</p>
+        </div>
+      </div>
+      <div v-else>
+        <p>加载中...</p>
+      </div>
+    </div>
+  </div>
+</template>
 
 <style scoped></style>
